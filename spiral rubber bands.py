ab1da1afa27a352630006745dc6406a406763696
awang1991 <33324103+awang1991@users.noreply.github.com>
2017-11-03 11:00:50 -0400

    Add files via upload

---
+++ spiral rubber bands.py
@@ -0,0 +1,434 @@
+from turtle import*
+bgcolor('orange')
+speed(-1)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(40)
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(59)
+
+
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(40):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(8)
+
+
+from turtle import*
+colors = ['orange','black','yellow','red','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(100)
+    left(59)
+from turtle import*
+colors = ['purple','black','yellow','red','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(30)
+    left(49)
+from turtle import*
+colors = ['orange','black','yellow','red','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(100)
+    left(59)
+from turtle import*
+colors = ['purple','black','yellow','red','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(30)
+    left(49)
+from turtle import*
+colors = ['orange','black','yellow','red','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(100)
+    left(59)
+from turtle import*
+colors = ['purple','black','yellow','red','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(30)
+    left(49)
+from turtle import*
+colors = ['orange','black','yellow','red','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(100)
+    left(59)
+from turtle import*
+colors = ['purple','black','yellow','red','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(30)
+    left(49)
+from turtle import*
+colors = ['orange','black','yellow','red','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(100)
+    left(59)
+
+
+
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(40):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    left(8)
+
+
+from turtle import*
+colors = ['orange','black','yellow','red','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(100)
+    left(59)
+from turtle import*
+colors = ['purple','black','yellow','red','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(30)
+    left(49)
+from turtle import*
+colors = ['orange','black','yellow','red','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(100)
+    left(59)
+from turtle import*
+colors = ['purple','black','yellow','red','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(30)
+    left(49)
+from turtle import*
+colors = ['orange','black','yellow','red','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(100)
+    left(59)
+from turtle import*
+colors = ['purple','black','yellow','red','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(30)
+    left(49)
+from turtle import*
+colors = ['orange','black','yellow','red','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(100)
+    left(59)
+from turtle import*
+colors = ['purple','black','yellow','red','black']
+for x in range(50):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(30)
+    left(49)
+from turtle import*
+colors = ['orange','black','yellow','red','black']
+for x in range(100):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(100)
+    left(59)
+
+from turtle import*
+colors = ['aqua','black','lime green','salmon','black']
+for x in range(40):
+    pencolor(colors[x%5])
+    width(x/100+1)
+    forward(x)
+    right(180)
+
